a75eed9ebfd70254dd4116a39ffaf037b0a4319a
Mauro Vargas <maurovargas7725@gmail.com>
2025-07-27 20:47:48 -0600

    First commit. Created Mountain file route

---
+++ src/app/mountain/page.js
@@ -0,0 +1,10 @@
+const Page = () =>{
+    return (
+        <>
+            <div className="flex min-h-screen flex-col items-center justify-center p-24">
+                <h1>Mountain Info</h1>
+            </div>
+        </>
+    );
+}
+export default Page;
